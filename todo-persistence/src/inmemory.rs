dbe514ca2976c55b32d9d9332942a41fcc554b00
shhr1214 <souhei0079@gmail.com>
2019-06-10 03:47:00 +0900

    inmemory の repository を作ってみたり

---
+++ todo-persistence/src/inmemory.rs
@@ -0,0 +1,58 @@
+use std::collections::HashMap;
+
+use todo_rs::{AppError, CommandResult, QueryResult, Task, TaskRepository};
+
+pub struct InMemoryTaskRepository {
+    tasks: HashMap<u64, Task>,
+}
+
+impl InMemoryTaskRepository {
+    pub fn new() -> InMemoryTaskRepository {
+        InMemoryTaskRepository {
+            tasks: HashMap::new(),
+        }
+    }
+}
+
+impl TaskRepository for InMemoryTaskRepository {
+    fn list(&self) -> QueryResult<Vec<Task>> {
+        Ok(self.tasks.iter().map(|(_, v)| v.clone()).collect())
+    }
+
+    fn find(&self, id: u64) -> QueryResult<&Task> {
+        match self.tasks.get(&id) {
+            Some(task) => Ok(task),
+            _ => Err(AppError::NotFound),
+        }
+    }
+
+    fn add(&mut self, task: Task) -> CommandResult {
+        let id = task.id();
+        if self.tasks.contains_key(&id) {
+            return Err(AppError::AlreadyExists(id));
+        }
+
+        self.tasks.insert(task.id(), task);
+        Ok(())
+    }
+
+    fn update(&mut self, task: Task) -> CommandResult {
+        let id = task.id();
+        if self.tasks.contains_key(&id) {
+            return Err(AppError::AlreadyExists(id));
+        }
+
+        self.tasks.insert(task.id(), task);
+        Ok(())
+    }
+
+    fn delete(&mut self, task: Task) -> CommandResult {
+        let id = task.id();
+        if !self.tasks.contains_key(&id) {
+            return Err(AppError::NotFound);
+        }
+
+        self.tasks.remove(&task.id());
+        Ok(())
+    }
+}
