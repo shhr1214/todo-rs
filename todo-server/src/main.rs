f375f856f66b13e0fd1d5c61aa45d03098f17c38
shhr1214 <souhei0079@gmail.com>
2019-11-07 01:28:11 +0900

    actix_web の基本的な使い方がわかってきた

--- todo-server/src/main.rs
+++ todo-server/src/main.rs
@@ -1,5 +1,3 @@
-use todo_rs::Task;
-
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use serde::Serialize;
 
@@ -16,6 +14,36 @@ fn list_tasks(_req: HttpRequest) -> impl Responder {
     tasks.push(Task::new("test4".into()));
 
     Tasks { tasks: tasks }
+}
+
+fn get_tasks(req: HttpRequest) -> impl Responder {
+    let id: &str = req.match_info().get("id").unwrap();
+    let id = id.to_owned();
+    println!("{}", id);
+
+    Task::new("test1".into())
+}
+
+#[derive(Clone, Debug, Serialize)]
+struct Task(todo_rs::Task);
+
+impl Task {
+    fn new(name: String) -> Self {
+        Task(todo_rs::Task::new(name))
+    }
+}
+
+impl Responder for Task {
+    type Error = Error;
+    type Future = Result<HttpResponse, Error>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = serde_json::to_string(&self)?;
+
+        Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body))
+    }
 }
 
 #[derive(Serialize)]
@@ -41,6 +69,7 @@ fn main() {
         App::new()
             .route("/", web::get().to(index))
             .route("/tasks", web::get().to(list_tasks))
+            .route("/tasks/{id}", web::get().to(get_tasks))
     })
     .bind("127.0.0.1:8088")
     .unwrap()
